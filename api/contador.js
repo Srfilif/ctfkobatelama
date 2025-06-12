460c84ff3617cc39d33111f62c81c8396d06fb81
jcp2612-oficial <cuentajuegoschaco@gmail.com>
2025-06-11 22:52:02 -0300

    Update contador.js

--- api/contador.js
+++ api/contador.js
@@ -1,7 +1,7 @@
 let visitas = 0;
 
 export default function handler(req, res) {
-  if(req.headers['origin'] == null || req.headers['origin'] != "https://www.kobatelama.site/"){
+  if(req.headers['origin'] == null || req.headers['origin'] != "www.kobatelama.site"){
     return;
   }
   visitas++; 
