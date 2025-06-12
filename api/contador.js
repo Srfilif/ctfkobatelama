9c3830204b9bbed138766186597f67e40c20c961
jcp2612-oficial <cuentajuegoschaco@gmail.com>
2025-06-11 23:10:50 -0300

    Update contador.js

--- api/contador.js
+++ api/contador.js
@@ -1,14 +1,6 @@
 let visitas = 0;
-console.log("hola");
 
 export default function handler(req, res) {
-  const origin = req.headers['origin'];
-
-  if (origin || !origin.includes("kobatelama.site")) {
-    res.status(403).json({ error: "Origen no permitido" });
-    return;
-  }
-
   visitas++; 
   res.status(200).json({ visitas });
 }
