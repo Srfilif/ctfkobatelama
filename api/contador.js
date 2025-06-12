31f006043947df5fd2d5398dcf8be26fb36923bb
jcp2612-oficial <cuentajuegoschaco@gmail.com>
2025-06-11 22:49:49 -0300

    Update contador.js

--- api/contador.js
+++ api/contador.js
@@ -1,6 +1,9 @@
 let visitas = 0;
 
 export default function handler(req, res) {
+  if(req.headers['origin'] == null || req.headers['origin'] != "Tu dominio"){
+    return;
+  }
   visitas++; 
   res.status(200).json({ visitas });
 }
