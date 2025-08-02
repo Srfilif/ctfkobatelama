cef10c85a3526a4a0f549d7019968443905f6c04
jcp2612-oficial <cuentajuegoschaco@gmail.com>
2025-08-02 00:57:34 -0300

    Add files via upload

---
+++ kobar/script.js
@@ -0,0 +1,78 @@
+fetch('webs.json')
+  .then(response => response.json())
+  .then(data => {
+ 
+var x = data
+alert("este sitio esta en desarrollo, automaticamente luego de aceptar esta alerta usted se hace responsable del sitio, las web que se encuentran nuestra web no tienen nada que ver con nuestro sitio solo tomamos su link y su descripcion, aclarado esto puede ingresar al sitio con su resposabilidad")
+
+function buscar() {
+document.querySelectorAll("a").forEach(function(o) {
+  o.remove();
+});
+var input = document.getElementById("input").value.trim().toLowerCase();
+
+
+
+
+  for (const  C in x) {
+for (let i = 0; i < x[C].length; i++) {
+var f = x[C][i].split("|")    
+var link=f[0]
+var linknombre = link.substring(0, link.lastIndexOf("."));
+var mensaje = f[1].trim().toLowerCase();
+
+
+
+if (input===link) {
+var a = document.createElement("a")
+a.href=link
+a.textContent=mensaje+"(web)"
+document.body.appendChild(a)
+}
+
+
+if (input.includes(mensaje)) {
+var a = document.createElement("a")
+a.href=link
+a.textContent=mensaje
+document.body.appendChild(a)
+}
+
+
+var M= mensaje.split(" ")
+var hh =false 
+for (let w = 0; w < M.length; w++) {
+if (M[w].length > 3 && (M[w].startsWith(input) || input.startsWith(M[w]))) {
+var a = document.createElement("a")
+a.href=link
+a.textContent=mensaje
+document.body.appendChild(a)
+hh=true
+break
+
+}
+
+}
+
+}
+}
+
+if(!hh){
+console.log("huy!")
+return
+}
+}
+
+window.addEventListener("keydown",(e)=>{
+if (e.keyCode===13) {
+buscar()
+}
+})
+
+  })
+
+document.getElementById("boton").addEventListener("click",()=>{
+buscar()
+})
+
+  //  fecha finalizacion  de extraccion de info con autorizacon del robots.txt":"1 agosto de 2025 2:17 AM (horario argentino)
