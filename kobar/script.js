5922febe6de9ead102ff431047ea5df106299628
jcp2612-oficial <cuentajuegoschaco@gmail.com>
2025-08-08 01:01:55 -0300

    Update script.js

--- kobar/script.js
+++ kobar/script.js
@@ -1,5 +1,40 @@
+var wxs=0 
+function webs() {
+  wxs=0
+  document.querySelectorAll("a").forEach(function(o) {
+  o.remove();
+});
+document.querySelectorAll("img").forEach(function(O) {
+if (!img.id !=="busqueda") {
+  img.remove();
+}
+});
+}
+function imga() {
+  wxs=1
+  document.querySelectorAll("a").forEach(function(o) {
+  o.remove();
+});
+document.querySelectorAll("img").forEach(function(O) {
+if (!img.id !=="busqueda") {
+  img.remove();
+}
+});
+}
+function documen() {
+  wxs=2
+  document.querySelectorAll("a").forEach(function(o) {
+  o.remove();
+});
+document.querySelectorAll("img").forEach(function(O) {
+if (!img.id !=="busqueda") {
+  img.remove();
+}
+});
+}
 window.addEventListener("DOMContentLoaded", function () {
 var adsc= 0
+
 
 
 fetch('webs.json')
@@ -33,47 +68,28 @@ buscar()
 
 
 function buscar() {
-adsc +=1
- if (adsc=== 5 ) {
-document.getElementById("patrocinado").remove()
-document.getElementById("ads").remove()
- }
 
-document.querySelectorAll("a").forEach(function(o) {
-  o.remove();
-});
+  if (wxs===0) {
+    
 var input = document.getElementById("input").value.trim().toLowerCase();
-
-
-
-
-
-
   for (const  C in x) {
 for (let i = 0; i < x[C].length; i++) {
 var f = x[C][i].split("|")    
 var link=f[0]
 var linknombre = link.substring(0, link.lastIndexOf("."));
 var mensaje = f[1].trim().toLowerCase();
-
-
-
 if (input===link) {
 var a = document.createElement("a")
 a.href=link
 a.textContent=mensaje+"(web)"
 document.body.appendChild(a)
 }
-
-
 if (input.includes(mensaje)) {
 var a = document.createElement("a")
 a.href=link
 a.textContent=mensaje
 document.body.appendChild(a)
 }
-
-
 var M= mensaje.split(" ")
 var hh =false 
 for (let w = 0; w < M.length; w++) {
@@ -83,19 +99,95 @@ a.href=link
 a.textContent=mensaje
 document.body.appendChild(a)
 hh=true
-break
-
-}
-
-}
-
-}
-}
-
+break}}}}
 if(!hh){
 console.log("huy!")
 return
 }
+  }
+
+
+
+  fetch('imgLI.json')
+  .then(response => response.json())
+  .then(data => {
+    var dato=data 
+if (wxs===1) {
+
+var input = document.getElementById("input").value.trim().toLowerCase();
+  for (const  C in dato) {
+for (let i = 0; i < dato[C].length; i++) {
+var f = dato[C][i].split("|")    
+var link=f[0]
+var linknombre = link.substring(0, link.lastIndexOf("."))
+var mensaje = f[1].trim().toLowerCase();
+if (input===link) {
+var img = document.createElement("img")
+img.alt=mensaje
+img.src=link
+const Ymax = 800;
+const Xmax =800;
+img.onload = () => {
+  let widthImg = img.naturalWidth;
+  let heightImg = img.naturalHeight;
+if (widthImg >Xmax || heightImg> Ymax)  {
+img.style.height=heightImg*50+"px"
+img.style.width=widthImg*50+"px"
+}
+}
+//img.style.background="black"
+document.body.appendChild(img)
+}
+if (input.includes(mensaje)) {
+var img = document.createElement("img")
+img.src=link
+const Ymax = 800;
+const Xmax =800;
+img.onload = () => {
+  let widthImg = img.naturalWidth;
+  let heightImg = img.naturalHeight;
+if (widthImg >Xmax ||heightImg> Ymax)  {
+img.style.height=heightImg*50+"px"
+img.style.width=widthImg*50+"px"
+}
+}
+//img.style.background="black"
+document.body.appendChild(img)
+}
+var M= mensaje.split(" ")
+var hh =false 
+for (let w = 0; w < M.length; w++) {
+if (M[w].length > 3 && (M[w].startsWith(input) || input.startsWith(M[w]))) {
+var img = document.createElement("img")
+img.src=link
+const Ymax = 800;
+const Xmax =800;
+img.onload = () => {
+  let widthImg = img.naturalWidth;
+  let heightImg = img.naturalHeight;
+if (widthImg >Xmax || heightImg> Ymax)  {
+img.style.height=heightImg*50+"px"
+img.style.width=widthImg*50+"px"
+}
+}
+//img.style.background="black"
+img.alt=mensaje
+document.body.appendChild(img)
+hh=true
+break}}}}
+if(!hh){
+console.log("huy!")
+return
+}
+
+
+
+
+
+
+}
+ })
+
 }
 
 
@@ -104,12 +196,5 @@ return
 
 
  
-
-})
-
-
-
-
-
 
   //  fecha finalizacion  de extraccion de info con autorizacon del robots.txt":"1 agosto de 2025 2:17 AM (horario argentino)
